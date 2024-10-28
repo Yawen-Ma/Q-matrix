c4ee04ad2da3bfd5683b62192781dbe38e93b69d
Yawen Ma <y.ma24@lancaster.ac.uk>
2024-10-28 22:45:56 +0000

    I added the packages used for latent transition CDMs.

--- 00_packages.R
+++ 00_packages.R
@@ -10,3 +10,11 @@ library(cdmTools)
 library(edina)
 library(hmcdm)
 
+## Latent transition CDMs by Liang et al. (2023)
+library(LTCDM)
+
+## State sequence analysis
+library(TraMineR)
+
+## Fitting a CDM at each time point in latent transition models
+library(GDINA)
